e234761ab1a82751e98d320028a86897592330ed
Tyler <tyler.doudrick@gmail.com>
2020-04-07 11:11:18 -0400

    init solution

---
+++ CIS4330_COVID_App/CIS4330_COVID_App/Views/AboutPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CIS4330_COVID_App.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
