cdc54e4e21eb0a2b10e107888ef6f063071daa79
Tyler <tyler.doudrick@gmail.com>
2020-04-07 14:47:51 -0400

    proper init

--- CIS4330_COVID_App/CIS4330_COVID_App.Android/MainActivity.cs
+++ CIS4330_COVID_App/CIS4330_COVID_App.Android/MainActivity.cs
@@ -6,6 +6,14 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Plugin.Permissions;
+using Plugin.CurrentActivity;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using System.Net.Http;
+using System.Text;
+using MongoDB.Bson.IO;
+using System.Threading.Tasks;
 
 namespace CIS4330_COVID_App.Droid
 {
@@ -14,12 +22,48 @@ namespace CIS4330_COVID_App.Droid
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            CrossCurrentActivity.Current.Init(this, savedInstanceState);
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+
+            Task.Run(async () => {
+                try
+                {
+                    await PosttoAPI();
+                }
+                catch (System.OperationCanceledException ex)
+                {
+                    Console.WriteLine($"Text load cancelled: {ex.Message}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            });
+        }
+
+        private async Task<string> PosttoAPI()
+        {
+            var client = new HttpClient();
+            var content = new StringContent("test");
+            var result = await client.PostAsync("localhost:8080", content).ConfigureAwait(false);
+            if (result.IsSuccessStatusCode)
+            {
+                var tokenJson = await result.Content.ReadAsStringAsync();
+            }
+            return "test";
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
+        {
+            Plugin.Permissions.PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
+
+
 }
---
+++ CIS4330_COVID_Web/Person.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CIS4330_COVID_Web
+{
+    public class Person
+    {
+        public ObjectId Id { get; set; }
+        public string Name { get; set; }
+    }
+}
